3e77763fdf03247f39a25a59b424df086f584e87
ambesh987 <54496820+ambesh987@users.noreply.github.com>
2022-04-14 14:38:48 +0530

    added files

---
+++ main.js
@@ -0,0 +1,9 @@
+const div=require('./divide');
+const mul=require('./multiply');
+const add =require('./add');
+const sub= require('./subtract');
+
+console.log(div.divide(10,2));
+console.log(mul.multiply(10,2));
+console.log(add.add(10,2));
+console.log(sub.subtract(10,2));
